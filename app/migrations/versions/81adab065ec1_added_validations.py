5d7a0c1d6b2f08ae9d12be3a946aefe0829d5d60
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-10-01 23:45:17 +0300

    Added new migration

---
+++ app/migrations/versions/81adab065ec1_added_validations.py
@@ -0,0 +1,24 @@
+"""added validations
+
+Revision ID: 81adab065ec1
+Revises: 03b3cb4db169
+Create Date: 2023-10-01 23:44:06.777507
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '81adab065ec1'
+down_revision = '03b3cb4db169'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
